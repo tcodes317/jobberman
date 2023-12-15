0a561b337a3405557c0dc9014f2054de046e120c
tcodes317 <tcodes317@gmail.com>
2023-12-15 13:27:44 +0000

    Component Header

--- app/src/component/Header.jsx
+++ app/src/component/Header.jsx
@@ -1,8 +1,13 @@
 function Header(){
     return(
         <>
-            <div className="relative">
-                <div className=""></div>
+            <div className="relative flex items-center">
+                <div className="">
+                    <img src="https://www.jobberman.com/static-assets/img/ng/landscape.svg" alt="" />
+                </div>
+                <div className="">
+                    Michael
+                </div>
             </div>
         </>
     )
