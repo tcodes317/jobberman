f2e8f983182114d0928293fecb3bea5100427db9
Codes317 <120498081+tcodes317@users.noreply.github.com>
2023-12-15 13:13:51 +0000

    tailwindcss init

---
+++ app/src/component/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <>
+            <div className=""></div>
+        </>
+    )
+}
+export default Header;
---
+++ app/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    screens:{
+      xxxs: "150px",
+      xxs: "300px",
+      xs: "468px",
+      md: "992px",
+      lg: "1024px",
+      xl: "1440px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
